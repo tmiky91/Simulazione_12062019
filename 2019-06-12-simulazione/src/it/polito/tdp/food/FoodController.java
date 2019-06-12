add9d476bf5ab70bb4db844b04a5433d1dc588f3
S173991 <S173991@lepcopi35.cdsdom.polito.it>
2019-06-12 12:02:32 +0200

    Master con aggiunto txtResult

--- 2019-06-12-simulazione/src/it/polito/tdp/food/FoodController.java
+++ 2019-06-12-simulazione/src/it/polito/tdp/food/FoodController.java
@@ -1,40 +1,36 @@
-/**
- * Sample Skeleton for 'Food.fxml' Controller Class
- */
-
 package it.polito.tdp.food;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-
-import it.polito.tdp.food.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FoodController {
-	
-	private Model model;
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // fx:id="txtCalorie"
-    private TextField txtCalorie; // Value injected by FXMLLoader
+    @FXML
+    private TextField txtCalorie;
 
-    @FXML // fx:id="btnCreaGrafo"
-    private Button btnCreaGrafo; // Value injected by FXMLLoader
+    @FXML
+    private Button btnCreaGrafo;
 
-    @FXML // fx:id="boxIngrediente"
-    private ComboBox<?> boxIngrediente; // Value injected by FXMLLoader
+    @FXML
+    private ComboBox<?> boxIngrediente;
 
-    @FXML // fx:id="btnDietaEquilibrata"
-    private Button btnDietaEquilibrata; // Value injected by FXMLLoader
+    @FXML
+    private Button btnDietaEquilibrata;
+
+    @FXML
+    private TextArea txtResult;
 
     @FXML
     void doCalcolaDieta(ActionEvent event) {
@@ -46,15 +42,13 @@ public class FoodController {
 
     }
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
     void initialize() {
         assert txtCalorie != null : "fx:id=\"txtCalorie\" was not injected: check your FXML file 'Food.fxml'.";
         assert btnCreaGrafo != null : "fx:id=\"btnCreaGrafo\" was not injected: check your FXML file 'Food.fxml'.";
         assert boxIngrediente != null : "fx:id=\"boxIngrediente\" was not injected: check your FXML file 'Food.fxml'.";
         assert btnDietaEquilibrata != null : "fx:id=\"btnDietaEquilibrata\" was not injected: check your FXML file 'Food.fxml'.";
-    }
-    
-    public void setModel(Model model) {
-    	this.model = model;
+        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Food.fxml'.";
+
     }
 }
